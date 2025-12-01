e56e04eca3943b8166ac5b1deb27cd859b8efe17
Luan Victor <lvictor.dutra@gmail.com>
2025-12-01 00:45:06 -0300

    feat: implement database connection function

---
+++ internal/infra/database/connection.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func DatabaseConnection(connectionString string, databaseName string) (*pgx.Conn, error) {
+	connection, err := pgx.Connect(context.Background(), connectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := connection.Ping(context.Background()); err != nil {
+		return nil, err
+	}
+
+	return connection, nil
+}
